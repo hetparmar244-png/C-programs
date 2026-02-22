b56aa2019eb53f77cb22f40154b1ee05e48dae08
het parmar <hetparmar244@gmail.com>
2026-02-22 23:04:47 +0530

    make a squre patten using c
    make a soildrectangle patten using c

---
+++ squrestar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main() {
+
+    int i,j,n;
+
+    printf("enter the number of row: ");
+    scanf("%d",&n);
+
+    for(i=1; i<=n; i++) {
+        for (j=1; j<=n; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+        
+    }
+}
